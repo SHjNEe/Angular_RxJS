900f843a8610b120ecd2f7e4df176a34aa664b68
SHjNEe <baotrung.jpvn@gmail.com>
2023-05-10 01:10:26 +0700

    Add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+("@angular/core");
+import { Subscription, interval } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  private firstObsSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
-
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
 }

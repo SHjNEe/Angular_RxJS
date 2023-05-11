4e36e7e350a7843d226b5defa42b0102cdd6402e
SHjNEe <baotrung.jpvn@gmail.com>
2023-05-11 23:29:57 +0700

    add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 ("@angular/core");
-import { Subscription, interval } from "rxjs";
+import { Observable, Subscription, interval, observable } from "rxjs";
 
 @Component({
   selector: "app-home",
@@ -9,11 +9,22 @@ import { Subscription, interval } from "rxjs";
 })
 export class HomeComponent implements OnInit {
   private firstObsSubscription: Subscription;
+  count: number;
   constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    let count = 0;
+    const customIntervalObservable = Observable.create((observer) => {
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+    this.firstObsSubscription = customIntervalObservable.subscribe((data) => {
+      this.count = data;
     });
   }
   ngOnDestroy() {
